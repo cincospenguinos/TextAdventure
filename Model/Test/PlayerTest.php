1d521570abee4c65af81786b76f13dda94b2638f
Andre LaFleur <andre.lafleur@utah.edu>
2016-05-24 21:50:55 -0600

    Add composer stuff and PhPUnit test structure.

---
+++ Model/Test/PlayerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tsvetok
+ * Date: 5/24/16
+ * Time: 9:49 PM
+ */
+
+namespace LinkedWorldsCoreTest;
+
+
+class PlayerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ Model/Test/RoomTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tsvetok
+ * Date: 5/24/16
+ * Time: 9:50 PM
+ */
+
+namespace LinkedWorldsCoreTest;
+
+
+class RoomTest extends \PHPUnit_Framework_TestCase
+{
+
+}
