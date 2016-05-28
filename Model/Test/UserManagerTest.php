d4f387c24c4c09e46f76fdef27bc17ed63d68e2c
Andre LaFleur <andre.lafleur@utah.edu>
2016-05-28 14:09:29 -0600

    Fixed infinite loop

---
+++ Model/Test/UserManagerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: tsvetok
+ * Date: 5/28/16
+ * Time: 11:11 AM
+ */
+include_once '../UserManager.php';
+include_once '../../dbconfig.php';
+
+class UserManagerTest extends PHPUnit_Framework_TestCase
+{
+    private $dbConnection;
+
+    public function setUp(){
+        $this->dbConnection = getDBConnection();
+    }
+
+    public function tearDown(){
+//        $statement = $this->dbConnection->prepare("DELETE FROM Users");
+//        $statement->execute();
+
+        unset($dbConnection);
+    }
+
+    /**
+     * If a user is thrown into the Users table, then I expect it to be there and UserManager::userExists()
+     * to find it.
+     */
+    public function testUserExists(){
+        $username = 'poop';
+        $hashword = password_hash('balls', PASSWORD_BCRYPT);
+        $salt = substr($hashword, 7, 22);
+
+        $statement = $this->dbConnection->prepare("INSERT INTO Users VALUES(:username, :hashword, :salt, NOW())");
+        $statement->bindParam(':username', $username);
+        $statement->bindParam(':hashword', $hashword);
+        $statement->bindParam(':salt', $salt);
+        $statement->execute();
+
+        $this->assertTrue(UserManager::userExists($username, $this->dbConnection));
+    }
+
+    /**
+     * When I attempt to create a new user, I expect that user to be there.
+     */
+    public function testCreateNewUser(){
+        $username = UserManager::createNewUser($this->dbConnection)[0];
+        $statement = $this->dbConnection->prepare("SELECT * FROM Users WHERE username=:username");
+        $statement->bindParam(':username', $username);
+        $result = $statement->fetch();
+
+        $this->assertTrue(isset($result));
+        $this->assertTrue(UserManager::userExists($username, $this->dbConnection));
+    }
+
+    /**
+     * When I create a new account out of an old guest user, I expect that account to be there.
+     */
+    public function testCreateNewAccount(){
+        $newUsername = 'herp';
+
+        $oldUsername = UserManager::createNewUser($this->dbConnection)[0];
+        $isSuccessful = UserManager::createNewAccount($oldUsername, $newUsername, 'derp', $this->dbConnection);
+
+        $this->assertTrue(isset($isSuccessful));
+
+        $statement = $this->dbConnection->prepare("SELECT * FROM Users WHERE username=:username");
+        $statement->bindParam(':username', $newUsername);
+        $result = $statement->fetch();
+
+        $this->assertTrue(isset($result));
+    }
+}
---
+++ Model/UserManager.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Class that contains all of the various methods that manage users. Handles all of the memcaching and database lookup
+ * information necessary for Users.
+ *
+ * TODO: Create a series of test cases for this class. This one needs to be tested to death.
+ *
+ * User: tsvetok
+ * Date: 5/27/16
+ * Time: 12:55 PM
+ */
+class UserManager
+{
+
+
+    public static function isUserLoggedIn($username){
+        // TODO: This
+    }
+
+    /**
+     * Creates a new user and stores that user into the database. Returns an array containing the username, password
+     * and salt of the user in that order, or null if an error occurred.
+     *
+     * NOTE: What happens if the $username is taken?
+     *
+     * @param $dbConnection
+     * @return array
+     */
+    public static function createNewUser($dbConnection){
+        $username = self::generateRandomUsernameOrPassword();
+
+        while(self::userExists($username, $dbConnection))
+            $username = self::generateRandomUsernameOrPassword();
+
+        $password = self::generateRandomUsernameOrPassword();
+        $hashword = password_hash($password, PASSWORD_BCRYPT);
+        $salt = substr($hashword, 7, 22);
+
+        try{
+            $statement = $dbConnection->prepare("INSERT INTO Users VALUES(:username, :hashword, :salt, NOW())");
+            $statement->bindValue(':username', $username);
+            $statement->bindValue(':hashword', $hashword);
+            $statement->bindValue(':salt', $salt);
+            $statement->execute();
+
+            $affectedRows = $statement->rowCount();
+
+            return [$username, $password, $salt];
+        } catch(PDOException $ex){
+            error_log('PDO Exception occurred!');
+            error_log($ex->errorInfo);
+            return null;
+        }
+    }
+
+    /**
+     * Creates a new account given the old username, new username, and new password for a given user. Returns
+     * null if there was an error, or a boolean indicating whether the insertion was successful or not.
+     *
+     * @param $oldUsername
+     * @param $newUsername
+     * @param $newPassword
+     * @param $dbConnection
+     * @return bool|null
+     */
+    public static function createNewAccount($oldUsername, $newUsername, $newPassword, $dbConnection){
+        if(!self::userExists($oldUsername, $dbConnection))
+            return false;
+
+        $hashword = password_hash($newPassword, PASSWORD_BCRYPT);
+        $salt = substr($hashword, 7, 22);
+
+        try {
+            $statement = $dbConnection->prepare("UPDATE Users SET username=:newUsername, hashword=:hashword, salt=:salt
+                                            WHERE username=:oldUsername");
+            $statement->bindParam([$newUsername, $hashword, $salt, $oldUsername]);
+            $statement->execute();
+
+            return self::userExists($newUsername, $dbConnection);
+        } catch(PDOException $ex){
+            error_log('PDO Exception occurred!');
+            error_log($ex->errorInfo);
+            return null;
+        }
+    }
+
+    /**
+     * Checks to see if the user matching the username passed exists. Returns null if an error occurred,
+     * or a boolean value indicating whether or not the user exists.
+     *
+     * @param $username
+     * @param $dbConnection
+     * @return null|bool
+     */
+    public static function userExists($username, $dbConnection){
+        try {
+            $statement = $dbConnection->prepare("SELECT * FROM Users WHERE username=:username");
+            $statement->bindValue(':username', $username);
+            $statement->execute();
+            $result = $statement->fetch();
+
+            if($result == false)
+                return false;
+
+            return true;
+        } catch(PDOException $ex) {
+            error_log('PDO Exception occurred!');
+            error_log($ex->errorInfo);
+
+            return null;
+        }
+    }
+
+    /**
+     * Generates a random username or password.
+     *
+     * @param int $length
+     * @return string
+     */
+    private static function generateRandomUsernameOrPassword($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
