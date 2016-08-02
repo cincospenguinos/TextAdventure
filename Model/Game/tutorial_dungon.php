cb4cc827d7cad7be2dc58e45e50a19eb8950016e
Andre LaFleur <andre.lafleur@utah.edu>
2016-08-01 21:15:20 -0600

    Create new tutorial dungeon to fit lore

--- Model/Game/tutorial_dungon.php
+++ Model/Game/tutorial_dungon.php
@@ -12,87 +12,72 @@ require_once 'Item.php';
 require_once 'Direction.php';
 
 // Setup all of the rooms
-$frontOfHouse = new \LinkedWorldsCore\Room('Front of White House', 'You are in front of a white house. Its front door is nailed shut.');
-$backOfHouse = new \LinkedWorldsCore\Room('Back of White House', 'You are in back of a white house. A window into it is open.');
-$houseKitchen = new \LinkedWorldsCore\Room('Kitchen', 'You are inside a simple kitchen, with bare cupboards and shelves.');
-$cellar = new \LinkedWorldsCore\Room('Cellar', 'You are in the cellar of the house. The air is cold and damp. You can see a dim light shimmering to the south west.');
-$shrine = new \LinkedWorldsCore\Room('Shrine', 'You are in front of a shrine. Long, slender white candles light the room.');
-$forestA = new \LinkedWorldsCore\Room('Forest', 'You are in a thick forest with branches all around you.');
-$forestB = new \LinkedWorldsCore\Room('Forest', 'You are in a thick forest with branches all around you.');
-$clearing = new \LinkedWorldsCore\Room('Clearing', 'You are standing in a clearing in the forest. You see an old stone well with what seems to be a ladder leading down inside it. You can hear the sound of crashing water from the south.');
-$waterfall = new \LinkedWorldsCore\Room('Waterfall', 'You are at the base of a waterfall. The cold water splashes and roars loudly in your ears.');
-$cliff = new \LinkedWorldsCore\Room('Cliff', 'You are standing on a cliff next to the waterfall.');
-$ravine = new \LinkedWorldsCore\Room('Large Ravine', 'You are in front of a large ravine, stretching far away. There is a bridge here that you can use to cross it.');
-$otherCliff = new \LinkedWorldsCore\Room('Cliff', 'You are standing at the cliff of a ravine. A slick, muddy slope leads towards the forest.');
-$darkLibrary = new \LinkedWorldsCore\Room('Dark Library', 'You are in what appears to be a library. The light is dim and books are stacked in high bookshelves.');
-$hallwayA = new \LinkedWorldsCore\Room('Hallway', 'You are in a regal looking hallway.');
-$kingdomKitchen = new \LinkedWorldsCore\Room('Kitchen', 'You are standing in a fancy kitchen, with all sorts of cooking equipment.');
-$banquetHall = new \LinkedWorldsCore\Room('Banquet Hall', 'You are in a bright banquet hall, with long oak tables and large, ornate chairs.');
-$mainHall = new \LinkedWorldsCore\Room('Main Hall', 'You are in a large, open room, in which guests fill with royal dances.');
-$hallwayB = new \LinkedWorldsCore\Room('Hallway', 'You are in a regal looking hallway.');
-$smallBedroom = new \LinkedWorldsCore\Room('Small Bedroom', 'You are standing in a small bedroom with a simple set of furniture.');
-$altar = new \LinkedWorldsCore\Room('Altar', 'You are standing in front of an altar, carved out of stone. The walls have carvings in them of mighty hands and wicked creatures.');
+$garden = new \LinkedWorldsCore\Room("Garden", "You wake up in a bright and green garden. Thick trees and bushes are scattered all about, " .
+    "with fruits of all kinds hanging off of them. There appears to be a beaten path in front of you, descending downward toward the bright " .
+    "sun in the east.");
+$frontOfHouse = new \LinkedWorldsCore\Room("Front of a House", "You are in front of a white house whose front door is nailed shut. The house appears " .
+    "to be abandoned, standing ominously in the dim light of the moon.");
+$backOfHouse = new \LinkedWorldsCore\Room("Back of a House", "You are in the back of the house. There is an open window which you can move through by going north.");
+$livingRoom = new \LinkedWorldsCore\Room("Living Room", "You are standing inside a living room. The moonlight shines upon the furniture, and exposes a trap door " .
+    "in the corner of the room.");
+$cellar = new \LinkedWorldsCore\Room("Cellar", "The cool, musty air rests on your shoulders as you descend into the cellar. There are wooden crates cast about the floor. " .
+    "You can see dim light emanating further down the cellar.");
+$mysticShrine = new \LinkedWorldsCore\Room("Secret Shrine", "You are standing in a room with a dirt floor, dug out through the back wall of the cellar. A stone statue, " .
+    "depicting a man holding a strange fruit, stands in the middle of the room.");
+$forest = new \LinkedWorldsCore\Room("Forest", "You are standing in a thick forest, where the moon is blocked out due to the tall trees. You see a light southwards in the distance.");
+$smallCabin = new \LinkedWorldsCore\Room("Small Cabin", "You are standing in front of a log cabin. Smoke is coming from its chimney. The door is open and there is a passage " .
+    "leading downwards inside.");
+$library = new \LinkedWorldsCore\Room("Library", "Mountains of ruined tomes flood the room, stacked and stuffed in bookshelves that loom above you.");
+$greatHall = new \LinkedWorldsCore\Room("Great Hall", "You are standing in a room that seems to have been a great hall for a king. Rubble blocks the path to the east wing, but a broken " .
+    "set of stairs leads upwards to what appears to be a throne room.");
+$throneRoom = new \LinkedWorldsCore\Room("Throne Room", "You are standing in front of an altar, in a destroyed thrown room. The altar seems to have been hastily put together, built of stone " .
+    "and mud. A portal, infinite and silent, swirls about on the north wall of the room. It beckons you in its silence; move forward only when ready.");
 
 // Put together all of the items.
-$artifact = new \LinkedWorldsCore\Item('small artifact', 'A small statue of a young boy holding a club in one hand and an olive branch in the other.');
-$artifact->addAlias('artifact');
+$strangeFruit = new \LinkedWorldsCore\Item("Strange Fruit", "A fist sized fruit, shaped like a sphere, with a smooth blue skin.");
+$strangeFruit->addAlias('fruit');
 
-$bookOfIncantations = new \LinkedWorldsCore\Item('Book of Incantations', 'It looks heavy and old, with yellowing pages and a damaged cover.');
-$bookOfIncantations->addAlias('book');
+$artifact = new \LinkedWorldsCore\Item("Artifact", "A small stone statue of a boy wearing an apron, holding a daggar and an acacia twig.");
 
-$club = new \LinkedWorldsCore\Item('Club', 'A heavy wooden club, neither carved nor extravagant. It\'s barely more than a stick, to be honest.', true);
+$tome = new \LinkedWorldsCore\Item("Spell Tome - Spirit Arrow", "A dusty tome describing the method to cast the spell Spirit Arrow.");
+$tome->addAlias('tome');
+$tome->addAlias('book');
+$tome->addAlias('spell book');
 
 // Throw in all the items
-$otherCliff->addItem($bookOfIncantations);
-$shrine->addItem($artifact);
-$houseKitchen->addItem($club);
+$garden->addItem($strangeFruit);
+$mysticShrine->addItem($artifact);
+$library->addItem($tome);
 
 // Connect all of the rooms
-$frontOfHouse->addExit(\LinkedWorldsCore\Direction::South, $backOfHouse);
-$frontOfHouse->addExit(\LinkedWorldsCore\Direction::East, $forestA);
-$backOfHouse->addExit(\LinkedWorldsCore\Direction::West, $frontOfHouse);
-$backOfHouse->addExit(\LinkedWorldsCore\Direction::North, $houseKitchen);
-$backOfHouse->addExit(\LinkedWorldsCore\Direction::East, $forestA);
-$houseKitchen->addExit(\LinkedWorldsCore\Direction::South, $backOfHouse);
-$houseKitchen->addExit(\LinkedWorldsCore\Direction::Down, $cellar);
-$cellar->addExit(\LinkedWorldsCore\Direction::Up, $houseKitchen);
-$cellar->addExit(\LinkedWorldsCore\Direction::SouthWest, $shrine);
-$shrine->addExit(\LinkedWorldsCore\Direction::NorthEast, $cellar);
-$forestA->addExit(\LinkedWorldsCore\Direction::West, $frontOfHouse);
-$forestA->addExit(\LinkedWorldsCore\Direction::South, $forestB);
-$forestB->addExit(\LinkedWorldsCore\Direction::West, $forestA);
-$forestB->addExit(\LinkedWorldsCore\Direction::East, $forestA);
-$forestB->addExit(\LinkedWorldsCore\Direction::South, $clearing);
-$clearing->addExit(\LinkedWorldsCore\Direction::SouthWest, $forestB);
-$clearing->addExit(\LinkedWorldsCore\Direction::Down, $darkLibrary);
-$clearing->addExit(\LinkedWorldsCore\Direction::South, $waterfall);
-$waterfall->addExit(\LinkedWorldsCore\Direction::NorthWest, $clearing);
-$waterfall->addExit(\LinkedWorldsCore\Direction::Up, $cliff);
-$cliff->addExit(\LinkedWorldsCore\Direction::South, $waterfall);
-$cliff->addExit(\LinkedWorldsCore\Direction::West, $ravine);
-$ravine->addExit(\LinkedWorldsCore\Direction::East, $cliff);
-$ravine->addExit(\LinkedWorldsCore\Direction::West, $otherCliff);
-$otherCliff->addExit(\LinkedWorldsCore\Direction::East, $ravine);
-$otherCliff->addExit(\LinkedWorldsCore\Direction::Down, $forestA);
-$darkLibrary->addExit(\LinkedWorldsCore\Direction::Up, $clearing);
-$darkLibrary->addExit(\LinkedWorldsCore\Direction::West, $hallwayA);
-$hallwayA->addExit(\LinkedWorldsCore\Direction::East, $darkLibrary);
-$hallwayA->addExit(\LinkedWorldsCore\Direction::South, $kingdomKitchen);
-$hallwayA->addExit(\LinkedWorldsCore\Direction::West, $mainHall);
-$kingdomKitchen->addExit(\LinkedWorldsCore\Direction::North, $hallwayA);
-$kingdomKitchen->addExit(\LinkedWorldsCore\Direction::West, $banquetHall);
-$mainHall->addExit(\LinkedWorldsCore\Direction::East, $hallwayA);
-$mainHall->addExit(\LinkedWorldsCore\Direction::South, $banquetHall);
-$mainHall->addExit(\LinkedWorldsCore\Direction::West, $hallwayB);
-$mainHall->addExit(\LinkedWorldsCore\Direction::North, $altar);
-$altar->addExit(\LinkedWorldsCore\Direction::South, $mainHall);
-$banquetHall->addExit(\LinkedWorldsCore\Direction::North, $mainHall);
-$banquetHall->addExit(\LinkedWorldsCore\Direction::East, $kingdomKitchen);
-$banquetHall->addExit(\LinkedWorldsCore\Direction::West, $hallwayB);
-$hallwayB->addExit(\LinkedWorldsCore\Direction::South, $banquetHall);
-$hallwayB->addExit(\LinkedWorldsCore\Direction::East, $mainHall);
-$hallwayB->addExit(\LinkedWorldsCore\Direction::West, $smallBedroom);
-$smallBedroom->addExit(\LinkedWorldsCore\Direction::East, $hallwayB);
+$garden->addExit(\LinkedWorldsCore\Direction::East, $frontOfHouse);
 
-// Ensure that the dungeon begins at the front of the house
-$dungeon = $frontOfHouse;
+$frontOfHouse->addExit(\LinkedWorldsCore\Direction::South, $backOfHouse);
+
+$backOfHouse->addExit(\LinkedWorldsCore\Direction::East, $frontOfHouse);
+$backOfHouse->addExit(\LinkedWorldsCore\Direction::West, $frontOfHouse);
+$backOfHouse->addExit(\LinkedWorldsCore\Direction::North, $livingRoom);
+$backOfHouse->addExit(\LinkedWorldsCore\Direction::South, $forest);
+
+$livingRoom->addExit(\LinkedWorldsCore\Direction::South, $backOfHouse);
+$livingRoom->addExit(\LinkedWorldsCore\Direction::Down, $cellar);
+
+$cellar->addExit(\LinkedWorldsCore\Direction::Up, $livingRoom);
+$cellar->addExit(\LinkedWorldsCore\Direction::Down, $mysticShrine);
+
+$mysticShrine->addExit(\LinkedWorldsCore\Direction::Up, $cellar);
+
+$forest->addExit(\LinkedWorldsCore\Direction::North, $backOfHouse);
+$forest->addExit(\LinkedWorldsCore\Direction::South, $smallCabin);
+
+$smallCabin->addExit(\LinkedWorldsCore\Direction::North, $forest);
+$smallCabin->addExit(\LinkedWorldsCore\Direction::Down, $library);
+
+$library->addExit(\LinkedWorldsCore\Direction::Up, $smallCabin);
+$library->addExit(\LinkedWorldsCore\Direction::East, $greatHall);
+
+$greatHall->addExit(\LinkedWorldsCore\Direction::West, $library);
+$greatHall->addExit(\LinkedWorldsCore\Direction::Up, $throneRoom);
+
+// Ensure that the dungeon begins at the garden
+$dungeon = $garden;
