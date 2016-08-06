f96e51229fa6204dcbf13b0c456aa8f3519dee26
Andre LaFleur <andre.lafleur@utah.edu>
2016-08-05 22:03:48 -0600

    Add a bunch of TODOs

--- Controller/Commands/attack.php
+++ Controller/Commands/attack.php
@@ -6,6 +6,10 @@
  *# <strong>attack [target]</strong> will throw a physical attack at the target provided.
  *
  * TODO: Figure out nice output for all the combat
+ * TODO: Disarming? Penalties on low rolls?
+ * TODO: Move attack method code out into the controller
+ * TODO: Add multiple strings to describe the action, rather than "so and so was hit"
+ * TODO: Smarter AI - if the AI drops its weapon, it picks it up. If the player drops its weapon, the AI picks it up.
  */
 $monsterName = str_replace('attack ', '', $command);
 $monsterName = str_replace('kill ', '', $monsterName);
